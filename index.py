1c35118ab7d1d761063bfd9120d5a828de566db9
Patricia Heimfarth <pbeier24@googlemail.com>
2021-11-21 12:04:05 +0100

    random points

---
+++ index.py
@@ -0,0 +1,26 @@
+import pygame
+import random
+
+screen = pygame.display.set_mode((500, 500))
+
+draw_on = False
+radius = 10
+
+
+try:
+    x = 0
+
+    while x < 100:
+
+        color = (random.randrange(256), random.randrange(
+            256), random.randrange(256))
+        pygame.draw.circle(screen, color, (random.randrange(
+            500), random.randrange(500)), radius)
+        draw_on = True
+        x = x + 1
+
+    pygame.image.save(screen, "test.jpg")
+
+
+except StopIteration:
+    pass
